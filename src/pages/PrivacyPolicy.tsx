273f67956a95241c97d34845de3bd5f74d71db99
Camila <camilaferna140494@gmail.com>
2025-11-17 22:56:06 -0300

    feat: create PrivacyPolicy page

---
+++ src/pages/PrivacyPolicy.tsx
@@ -0,0 +1,64 @@
+import { Box, Divider, Skeleton, Typography } from "@mui/material";
+import { getActiveTermsOfUse } from "../api/PrivacyPolicy/termsOfUse.api";
+import { useQuery } from "@tanstack/react-query";
+
+const PrivacyPolicy = () => {
+
+  	const {
+		data: termsOfUse,
+		isLoading,
+		isFetching,
+	} = useQuery({
+		queryKey: ["getActiveTermsOfUse"],
+		queryFn: () => getActiveTermsOfUse(),
+	});
+
+  return(
+  <Box  m={4}>
+    <Typography variant="h4" gutterBottom>
+      Política de Privacidade
+    </Typography>
+	
+
+  {isLoading || isFetching ? (
+								<>
+									{Array.from({ length: 20 }).map((_, index) => (
+										    <Skeleton variant="text" key={index}/>))}
+
+								</>
+							) : (
+								<>
+					
+									    <Typography variant="h5">{termsOfUse?.content.title}</Typography>
+
+									{termsOfUse?.content.intro?.map(
+										(paragraph: string, idx: number) => (
+											<Typography key={`intro-${idx}`} style={{ marginTop: 8, marginBottom: 8 }}>
+												{paragraph}
+											</Typography>
+										),
+									)}
+									{termsOfUse?.content.sections?.map(
+										(section: any, idx: number) => (
+											<Box key={`section-${idx}`} style={{ marginBottom: 16 }}>
+												<Typography style={{ marginBottom: 6 }} variant="h6">
+													{section.title}
+												</Typography>
+												{section.content.map((line: string, i: number) => (
+													<Typography
+														key={`content-${i}`}
+														style={{ marginBottom: 4 }}
+													>
+														- {line}
+													</Typography>
+												))}
+												<Divider />
+											</Box>
+										),
+									)}
+								</>
+							)}
+  </Box>
+)};
+
+export default PrivacyPolicy;
