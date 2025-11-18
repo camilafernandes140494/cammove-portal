dde7a15818d04223fad16fa61bfb6a11520604a7
Camila <camilaferna140494@gmail.com>
2025-11-17 22:59:43 -0300

    feat: implement main app structure with routing and API integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import theme from "./theme/theme";
+import Layout from "./components/Layout";
+import Support from "./pages/Support";
+import Marketing from "./pages/Marketing";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import { CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 5 * 60 * 1000, // 5 minutos
+    },
+  },
+});
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Marketing />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="/support" element={<Support />} />
+              <Route path="/marketing" element={<Marketing />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </ThemeProvider>
+      {/* DevTools opcional - ajuda a debugar */}
+    </QueryClientProvider>
+
+  );
 }
 
-export default App
+export default App;
