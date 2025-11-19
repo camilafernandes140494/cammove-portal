7ad9551883553915878a6abfdeb2ef978d427b7a
Camila <camilaferna140494@gmail.com>
2025-11-18 22:32:33 -0300

    fix: remove margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 import { Box, Typography } from "@mui/material";
 
 const Footer = () => (
-  <Box mt={5} py={3} textAlign="center" bgcolor="#f5f5f5">
+  <Box py={3} textAlign="center" bgcolor="#f5f5f5">
     <Typography variant="body2"> © {new Date().getFullYear()} CamMove — Todos os direitos reservados.
 </Typography>
   </Box>
